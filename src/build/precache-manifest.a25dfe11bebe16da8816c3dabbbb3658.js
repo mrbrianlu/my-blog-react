2819b0e77a2deda78acbb991934833389c5c38fb
Brian Lu <mrbrianlu@gmail.com>
2020-10-06 14:18:22 -0700

    First commit

---
+++ src/build/precache-manifest.a25dfe11bebe16da8816c3dabbbb3658.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6d5ff974be4808ef3cc23cfb35602ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e56c63cb4ba8a23f14d0",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "9f27527568c48733aa8c",
+    "url": "/static/js/2.ca0bc356.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ca0bc356.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e56c63cb4ba8a23f14d0",
+    "url": "/static/js/main.ee642cf4.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
